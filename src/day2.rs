7133b0281bd492dea86d06137e23ee1b7f5bc10c
Alain Jean PARES <ajp@free.fr>
2024-01-24 11:07:16 +0100

    day 2 part 2 ok

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,4 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
 
 #[aoc_generator(day2)]
 fn input_generator(input: &str) -> Vec<Vec<u32>> {
@@ -8,7 +7,10 @@ fn input_generator(input: &str) -> Vec<Vec<u32>> {
 
     for line in input.lines() {
         let s_line=line.split('\t');
-        v.push(s_line.into_iter().map(|v| v.parse::<u32>().unwrap()).collect::<Vec<u32>>());
+        let mut l:Vec<u32>=Vec::new();
+        l=s_line.into_iter().map(|v| v.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+        l.sort();
+        v.push(l);
     }
     println!("{:?}", v);
     v
@@ -19,6 +21,24 @@ fn solve_part1(input: &Vec<Vec<u32>>) -> u32 {
 
     input.iter().map(|l|{
         l.iter().max().unwrap()-l.iter().min().unwrap()
+    }).sum()
+}
+ 
+#[aoc(day2, part2)]
+fn solve_part2(input: &Vec<Vec<u32>>) -> u32 {
+
+    let mut v=input.clone();
+    v.iter().map(|l|{
+        let mut total=0;
+        for i in 0..l.len() {
+            for j in i+1..l.len() {
+                if l[j]%l[i]==0 {
+                    total+=l[j]/l[i];
+                    println!("found {} and {} on line {:?}", l[j], l[i], l);
+                }
+            }
+        }
+        total
     }).sum()
 }
  
