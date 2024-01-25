587d80e0a9bc35f1508999c63cbf6cb3a6b402e5
Alain Jean PARES <ajp@free.fr>
2024-01-25 11:12:30 +0100

    Day 4 part 2 ok

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::{Itertools, Permutations};
 
 #[aoc_generator(day4)]
 fn input_generator(input: &str) -> Vec<String> {
@@ -22,6 +23,29 @@ fn solve_part1(v: &Vec<String>) -> u32 {
         let mut hwords:HashSet<String>=HashSet::new();
         for w in line.split_whitespace() {
             if ! hwords.insert(w.to_string()) {
+                bgood=false;
+            }
+        }
+        if bgood {
+            total+=1;
+        }
+    }
+ total
+}
+
+#[aoc(day4, part2)]
+fn solve_part2(v: &Vec<String>) -> u32 {
+    let mut total:u32=0;
+
+    let mut total:u32=0;
+
+    for line in v {
+        let mut bgood=true;
+        let mut hwords:HashSet<String>=HashSet::new();
+        for w in line.split_whitespace() {
+            let s_word = w.chars().sorted().collect::<String>();
+
+            if ! hwords.insert(s_word) {
                 bgood=false;
             }
         }
