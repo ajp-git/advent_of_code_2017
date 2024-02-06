a625f18a9de39d02ec04fb6df8d25203a656a3bd
Alain Jean PARES <ajp@free.fr>
2024-02-06 09:32:47 +0100

    day 16 part 2 ok

--- src/day16.rs
+++ src/day16.rs
@@ -60,3 +60,39 @@ fn solve_part1(input: &Vec<Instruction>) -> String {
     }
     prog.into_iter().collect()
 }
+
+#[aoc(day16, part2)]
+fn solve_part2(input: &Vec<Instruction>) -> String {
+
+//    let mut prog="abcde".chars().collect::<Vec<char>>();
+    let mut prog="abcdefghijklmnop".chars().collect::<Vec<char>>();
+
+
+    // as every 30 is resets, 1_000_000_000%30=10
+    for t in 0..10 {
+        for ins in input.iter(){
+            match ins {
+                Instruction::Exc(a, b) => {
+                    let c=prog[*a as usize];
+                    prog[*a as usize]=prog[*b as usize];
+                    prog[*b as usize]=c;
+                },  
+                Instruction::Par(a, b) => {
+                    let fa=prog.iter().position(|c| c==a).unwrap();
+                    let fb=prog.iter().position(|c| c==b).unwrap();
+                    let c=prog[fa]; prog[fa]=prog[fb];prog[fb]=c;
+                },
+                Instruction::Spin(a) => {
+                    for _ in 0..*a as usize {
+                        let last=prog.len()-1;
+                        let c=prog[last];
+                        prog.remove(last);
+                        prog.insert(0, c);
+                    }
+                },         
+            }
+        }
+        println!("It : {t} :{}",prog.clone().into_iter().collect::<String>());
+    }
+    prog.into_iter().collect()
+}
