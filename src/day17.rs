dde0f24d11f68a164d70d4e4a4a782af0a5ab55f
Alain Jean PARES <ajp@free.fr>
2024-02-07 07:00:19 +0100

    day 17 part 1 ok

--- src/day17.rs
+++ src/day17.rs
@@ -8,7 +8,7 @@ fn input_generator(input: &str) -> u32 {
 
 #[aoc(day17, part1)]
 fn solve_part1(input: &u32) -> u32 {
-    let input:&u32=&3;
+    //let input:&u32=&3;
     let mut v:Vec<u32>=vec![0];
     let mut pos=0;
     for i in 1..=2017{
@@ -22,9 +22,6 @@ fn solve_part1(input: &u32) -> u32 {
             v.insert(pos, i as u32);
             
         }
-        if i <10 || i>2015{
-            println!("i={}\t{pos}\t{:?}", i,v);
-        }
-    }
+      }
     v[pos+1]
 }
