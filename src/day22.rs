45909d7fb32ec9023b6243a59c485d0c1e858cb5
Alain Jean PARES <ajp@free.fr>
2024-02-19 14:57:39 +0100

    part 1 test ok and display

--- src/day22.rs
+++ src/day22.rs
@@ -52,21 +52,68 @@ impl Direction {
             Direction::Right => Direction::Up 
         }
     }
+
+    fn forward (&self, (x,y):(i32,i32)) -> (i32,i32) {
+        match self {
+            Direction::Down => (x,y+1),
+            Direction::Up => (x,y-1),
+            Direction::Left => (x-1,y),
+            Direction::Right => (x+1,y),
+            
+        }
+    }
 }
 
 #[aoc(day22, part1)]
 fn solve_part1(h: &HashMap<(i32,i32), bool>) -> u32 {
 
+    let mut h=h.clone();
     // find middle to start
-    let start_x = (h.keys().map(|(x,_)| x ).max().unwrap()-
+    let mut x = (h.keys().map(|(x,_)| x ).max().unwrap()-
         h.keys().map(|(x,_)| x ).min().unwrap())/2;
 
-    let start_y = (h.keys().map(|(_,y)| y ).max().unwrap()-
+    let mut y = (h.keys().map(|(_,y)| y ).max().unwrap()-
         h.keys().map(|(_,y)| y ).min().unwrap())/2;
 
-    println!("Starting at ({},{})", start_x, start_y);
+    println!("Starting at ({},{})", x, y);
 
     let mut curr_dir=Direction::Up;
 
-    0
+    let mut infected=0;
+
+    for i in 0..10000 {
+        match h.get(&(x,y)).unwrap_or(&false) {
+            false =>{
+                curr_dir=curr_dir.left();
+                h.insert((x,y), true);
+                infected+=1;
+            },
+            true =>{
+                curr_dir=curr_dir.right();
+                h.insert((x,y), false);
+            },
+        }
+        (x,y)=curr_dir.forward((x,y));
+        if i%500==0{
+            println!("\nStep {i}");
+            diplay_grid(&h, x, y, 10);
+        }
+    }
+
+    infected
+}
+
+fn diplay_grid(h:&HashMap<(i32,i32), bool>, x:i32,y:i32,width:i32){
+
+    for dy in (y-width/2)..(y+width/2) {
+        for dx in (x-width/2)..(x+width/2) {
+            if x==dx && y==dy {
+                print!("{}",if h.get(&(dx,dy)).unwrap_or(&false) == &true {"[#]"} else{"[.]"});
+            } else {
+                print!("{}",if h.get(&(dx,dy)).unwrap_or(&false) == &true {" # "} else{" . "})                
+            }
+        }
+        println!();
+    }
+
 }
