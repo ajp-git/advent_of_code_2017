f0291d8db706282844a4ab067a267827cb55b8f1
Alain Jean PARES <ajp@free.fr>
2024-02-20 08:42:52 +0100

    part 1 ok

--- src/day24.rs
+++ src/day24.rs
@@ -21,7 +21,7 @@ fn input_generator(input: &str) -> Vec<(u8,u8)> {
 #[aoc(day24, part1)]
 fn solve_part1(input:&Vec<(u8,u8)>) -> u32 {
 
-    let res=connect(0, input);
+    let res=connect1(0, input, 0, 0);
 
     println!("{:?}", res);
     res.0
@@ -30,34 +30,59 @@ fn solve_part1(input:&Vec<(u8,u8)>) -> u32 {
 #[aoc(day24, part2)]
 fn solve_part2(input:&Vec<(u8,u8)>) -> u32 {
 
-    let res=connect(0, input);
+    let res=connect2(0, input, 0, 0);
 
     println!("{:?}", res);
     res.1
 }
 
 /// returns (weight, length) 
-fn connect(start: u8, avail: &Vec<(u8,u8)>) -> (u32, u32) {
+fn connect1(start: u8, avail: &Vec<(u8,u8)>, length:u32, weight:u32 ) -> (u32, u32) {
 
-    let mut weight:u32=0;
-    let mut length:u32=0;
-
-    if let Some((pos, next)) = avail.iter().enumerate().find(|(pos,(a,b))|a==&start||b==&start) {
-        
-        let mut shrink=avail.clone();
-        shrink.remove(pos);
-
-        weight+=(next.0+next.1) as u32;
-        length+=1;
-
-        let res;
-        if next.0 ==start {
-            res = connect(next.1, &shrink);
-        } else {
-            res = connect(next.0, &shrink);
-        }
-        return (res.0+weight,res.1+length);
-    } else {
-        return (weight,length)
+    if avail.is_empty() {
+        return (weight,length);
     }
+    
+    let mut max_weight=0;
+    let mut max_length=0;
+
+    for (pos, next) in avail.iter().enumerate() {
+        
+        if next.0 == start || next.1 == start {
+            let mut shrink=avail.clone();
+            let s = if next.0==start { next.1} else { next.0};
+            shrink.remove(pos);
+            
+            let res = connect1(s, &shrink, length+1, (next.0+next.1) as u32);
+            max_weight=max_weight.max(res.0);
+            max_length=max_length.max(res.1);
+        }
+    }
+    (weight+max_weight,length+max_length)
+}
+
+
+/// returns (weight, length) 
+fn connect2(start: u8, avail: &Vec<(u8,u8)>, length:u32, weight:u32 ) -> (u32, u32) {
+
+    if avail.is_empty() {
+        return (weight,length);
+    }
+    
+    let mut max_weight=0;
+    let mut max_length=0;
+
+    for (pos, next) in avail.iter().enumerate() {
+        
+        if next.0 == start || next.1 == start {
+            let mut shrink=avail.clone();
+            let s = if next.0==start { next.1} else { next.0};
+            shrink.remove(pos);
+            
+            let res = connect2(s, &shrink, length+1, (next.0+next.1) as u32);
+            max_weight=max_weight.max(res.0);
+            max_length=max_length.max(res.1);
+        }
+    }
+    (weight+max_weight,length+max_length)
 }
